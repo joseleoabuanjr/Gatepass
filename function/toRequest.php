dfde9e7407574ea1e64693a3ab578bb6e52d449d
leoabuan <119068420+snowblack01@users.noreply.github.com>
2022-11-30 03:27:11 +0800

    init commit

--- function/toRequest.php
+++ function/toRequest.php
@@ -4,8 +4,17 @@
 
     $id = $_GET['id'];
     $pps = $_POST['purpose'];
-    $date = $_POST['date'];
-    $req = "pending";
+    $date= $_POST['date'];
+    $scan_s = "denied";
+    $apt_s = "pending";
+    $exp = date_create($date);
+    
+    // Use date_add() function to add date object
+    date_add($exp, date_interval_create_from_date_string("1 days"));
+    
+    // Display the added date
+    $dexp = date_format($exp, "Y-m-d");
+
     //--CHECKING DATA
     //read data from database table;
     //to check if $id is in the database table;
@@ -21,6 +30,7 @@
                 $first = $row['first'];
                 $mid = $row['middle'];
                 $last = $row['last'];
+                $type = $row['type'];
         }
         $name = $first." ".$mid.". ".$last;
 
@@ -28,16 +38,18 @@
         if($pps == "Other"){
             $reason = $_POST["reason"];
             //insert data to appt_request table form user_account table;
-            $update = "UPDATE user_account SET req_status ='$req',reason ='$reason',date_apt ='$date' WHERE acc_no = $accno";
-            if(mysqli_query($connect,$update)){
+            $insert = "INSERT INTO appointment (acc_no,name,type,reason,date_apt,scan_stats,apt_stats,qr,apt_exp) VALUES ('$accno','$name','$type','$reason','$date','$scan_s','$apt_s','$qr','$dexp')";
+            if(mysqli_query($connect,$insert))
+            {
                 header("Location: ../index.php");
             }
         }
         else{
             $reason = $pps;
             //insert data to appt_request table form user_account table;
-            $update = "UPDATE user_account SET req_status ='$req',reason ='$reason',date_apt ='$date' WHERE acc_no = $accno";
-            if(mysqli_query($connect,$update)){
+            $insert = "INSERT INTO appointment (acc_no,name,type,reason,date_apt,scan_stats,apt_stats,qr,apt_exp) VALUES ('$accno','$name','$type','$reason','$date','$scan_s','$apt_s','$qr','$dexp')";
+            if(mysqli_query($connect,$insert))
+            {
                 header("Location: ../index.php");
             }
         }
