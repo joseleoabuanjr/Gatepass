fecc0aeb6be496c5df46a137dc2455ede85c84e7
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-02 12:48:53 +0800

    commit new

--- includes/profile.php
+++ includes/profile.php
@@ -23,17 +23,18 @@
 		$vax = $row["vax"];
 	}
 ?>
-<?php
-	if($type == "student"){
-		require_once 'profile_sx.php';
-	}
-	else if($type == "employee"){
-		require_once 'profile_e.php';
-	}
-	else if($type == "visitor"){
-		require_once 'profile_v.php';
-	}
-	else{
-		echo "<script>alert(Error)</script>";
-	}
-?>
+<div class="container d-flex" style="width: 100%; height: 100%; margin-top:100px;">
+<h1>Profile Information</h1>
+</div>
+<div class="container d-flex" style="width: 100%; height: 100%;">
+	<div class="d-flex justify-content-center align-items-center flex-column w-25 pt-5">
+		<div class="d-flex flex-column w-100 pe-2">
+			<button type="button" class="btn btn-primary btn-sm" id="infobtn1" style="margin-bottom:10px;">Information</button>
+			<button type="button" class="btn btn-primary btn-sm" id="infobtn2" style="margin-bottom:10px;">Avatar</button>
+			<button type="button" class="btn btn-primary btn-sm" id="infobtn3" style="margin-bottom:10px;">Credential</button>
+		</div>
+	</div>
+	<div class="d-flex">
+
+	</div>
+</div>
