960a354d1d0316bbebd8522919fb0f0ac510e8d4
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-05 18:58:39 +0800

    block user function

---
+++ function/toblock.php
@@ -0,0 +1,27 @@
+<?php
+    // //include qr api
+    // require_once "../phpqrcode/qrlib.php";
+    include 'connect.php';
+
+    $id = $_GET["id"];
+    $status = "blocked";
+
+    // $text = $id;	//Only the student number will  be saved in the QR Code;
+    //                 //If you want every information be stored in the QR Code use the code below instead;
+    // $idqr = $id.rand(10,99);
+    // $path = '../Images/'; 		//name of folder where to store all QR Images
+    // $file = $path.$idqr.".png"; 	//format of filename for each QR Images created. Ex: Images/2022123456.png;
+    // QRcode::png($text, $file, 'L', 5, 2); //generates QR Images, Parameters are (Text Contents, File Name, ECC, QRSize, FrameSize);
+
+    $update = "UPDATE user_account SET verification= '$status' WHERE acc_no = $id";
+
+    if(mysqli_query($connect,$update))
+    {
+        echo "<script>alert('Update Success.')</script>";
+        require "../email/userblocked.php";
+    }
+    else
+    {
+        echo "<script>alert('Update Failed.')</script>";	
+    }
+?>
