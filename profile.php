63145c79c811d5720e4423d34b972e496a28ac7e
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-07 14:47:45 +0800

    bug fixed

--- profile.php
+++ profile.php
@@ -57,6 +57,7 @@ $bday = date_format($date, "Y-m-d");
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/profile.css">
 
@@ -350,7 +351,7 @@ $bday = date_format($date, "Y-m-d");
                                         <h6>Vaccination Card</h6>
                                         <div class="input-group mb-1">
                                             <input type="text" class="form-control" value="<?php echo ($vax); ?>" disabled>
-                                            <a class='btn btn-secondary px-3' target="_blank" href='viewvax.php?id=<?php echo $vax ?>'>View</a>
+                                            <a class='btn btn-secondary px-3' target="_blank" href='viewvax.php?id=<?php echo $accno ?>'>View</a>
                                         </div>
                                         <div class="form-floating">
                                             <input type="file" name="vax" accept="application/pdf" class="form-control" id="vax1" style="height: 80px;padding-top:40px; padding-left:40px">
@@ -362,7 +363,7 @@ $bday = date_format($date, "Y-m-d");
                                         <h6>Valid ID Card</h6>
                                         <div class="input-group mb-1">
                                             <input type="text" class="form-control" value="<?php echo ($v_id); ?>" disabled>
-                                            <a class='btn btn-secondary px-3' target="_blank" href='view_vid.php?id=<?php echo $v_id ?>'>View</a>
+                                            <a class='btn btn-secondary px-3' target="_blank" href='view_vid.php?id=<?php echo $accno ?>'>View</a>
                                         </div>
                                         <div class="form-floating">
                                             <input type="file" name="vid" accept="application/pdf" class="form-control" id="vid1" style="height: 80px;padding-top:40px; padding-left:40px">
@@ -481,7 +482,7 @@ $bday = date_format($date, "Y-m-d");
     <footer class="py-4 mt-5 text-bg-dark text-center"></footer>
 
 	<!-- Javascripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
