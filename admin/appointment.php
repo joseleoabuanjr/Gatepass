9f9bf696dd6e3d86aa0fd7971184c2bc5652762e
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-12 17:36:49 +0800

    Update appointment.php

--- admin/appointment.php
+++ admin/appointment.php
@@ -71,8 +71,8 @@ if (!isset($_SESSION["useradmin"]) && !isset($_SESSION["passadmin"])) {
                                     <td>" . $row["date"] . "</td>
                                     <td>
                                     <div class='btn-group' role='group'>
-                                        <button type='button' class='btn btn-primary statusBtn btn-sm' data-status='approved' data-accno='" . $row['acc_no'] . "' data-reqid='" . $reqid . "'>Approve</button>
-                                        <button type='button' class='btn btn-danger statusBtn btn-sm' data-status='denied' data-accno='" . $row['acc_no'] . "' data-reqid='" . $reqid . "'>Deny</button>
+                                        <button type='button' class='btn btn-primary statusBtn btn-sm' data-status='approve' data-accno='" . $row['acc_no'] . "' data-reqid='" . $reqid . "'>Approve</button>
+                                        <button type='button' class='btn btn-danger statusBtn btn-sm' data-status='deny' data-accno='" . $row['acc_no'] . "' data-reqid='" . $reqid . "'>Deny</button>
                                     </div>
                                     </td>
                                 </tr>");
@@ -93,7 +93,7 @@ if (!isset($_SESSION["useradmin"]) && !isset($_SESSION["passadmin"])) {
             <div class="modal-content rounded-3">
                 <div class="modal-body p-4 text-center">
                     <h5 class="">Confirmation</h5>
-                    <p class="mb-1">Are you sure you want to set "<span class="status"></span>" on account No. <span id="accNoModal"></span></span>?</p>
+                    <p class="mb-1">Are you sure you want to <span class="status"></span> Account No. <span id="accNoModal"></span>?</p>
                     <!-- <p class="mb-0 text-danger fw-bolder">*This action is cannot be undone!</p> -->
                     <div class="alert alert-danger my-1" role="alert" id="errorAlert">
                         <span class="status text-capitalize"></span> Failed.
