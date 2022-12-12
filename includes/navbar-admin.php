29e8557165cb45c3172a9eb64d99d98d1956a92a
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-12 23:48:26 +0800

    commit

--- includes/navbar-admin.php
+++ includes/navbar-admin.php
@@ -1,3 +1,15 @@
+<?php
+include '../function/connect.php';
+$select = "SELECT * FROM admin_account Where type = 'superadmin'";
+$result = mysqli_query($connect, $select);
+$count =  mysqli_num_rows($result);
+
+if ($count == 1) {
+	while ($row = mysqli_fetch_assoc($result)) {
+		$type = $row['type'];
+	}
+}
+?>
 <nav class="navbar navbar-expand-lg sticky-top navbar-dark " style="background-color: #763435; height: 50px">
     <div class="container d-flex justify-content-between">
         <div class="navbar-brand" style="width:400px;"><span class="init">B</span>ULACAN <span class="init"> S</span>TATE <span class="init"> U</span>NIVERSITY <span class="init">[</span>GATEPASS<span class="init">]</span></div>
@@ -10,8 +22,23 @@
                 <a class="nav-link" href="verification.php" id="tl2">Verification</a>
                 <a class="nav-link" href="appointment.php" id="tl3">Appointments</a>
                 <a class="nav-link" href="time_inout.php" id="tl4">Time-in/out</a>
+                <?php 
+                    if($type == 'superadmin') { 
+                        echo ('
+                        <div class="nav-item dropdown" style="border-right: 1px gray solid; margin-right:5px;padding-right:5px;">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                More
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Admin</a></li>
+                                <li><a class="dropdown-item" href="../scanner.php">QR-Scanner</a></li>
+                            </ul>
+                        </div>');
+                    }
+                ?>
                 <a class="nav-link" href="../function/toLogout.php">Logout</a>
             </div>
+            
         </div>
     </div>
 </nav>
--- includes/nf_verified.php
+++
@@ -1,6 +0,0 @@
-<div class="d-flex flex-column" style="height:auto; padding:80px;">
-    <div class="alert alert-danger" role="alert">
-        This page is not available until your account is fully verified.
-    </div>
-    <div class="title"></div>
-</div>
