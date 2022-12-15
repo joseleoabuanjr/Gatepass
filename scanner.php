50c5f1b3979a410ea94d922ac5a8c5846e9dcfee
Leo2201 <119068420+snowblack01@users.noreply.github.com>
2022-12-15 21:23:58 +0800

    reload scanner every one minute

--- scanner.php
+++ scanner.php
@@ -6,6 +6,7 @@
 
 <head>
 	<title>QR Scanner Page</title>
+	<meta http-equiv="Refresh" content="60"> 
 	<!--CSS-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<!--javascript-->
